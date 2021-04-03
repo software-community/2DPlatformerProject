9e128a3b74e25bbbc040b77e4471cc4e88e2a261
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2021-04-03 17:08:00 +0530

    Session 1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D _rb;
+
+    private void Awake() {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        float horizontal = Input.GetAxis("Horizontal");
+        
+        Vector2 vel = _rb.velocity;
+        vel.x = horizontal * moveSpeed;
+        _rb.velocity = vel;
+        
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
