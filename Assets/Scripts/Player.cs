6ca5eef659b402248604b1919c47ab7b78695e2a
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2021-04-03 23:17:32 +0530

    Session 2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
-
+    public LayerMask groundLayers;
+    public int maxAirJumps;
+    
     private Rigidbody2D _rb;
 
+    private bool _isGrounded;
+    private int _airJumpsLeft;
+    
     private void Awake() {
         _rb = GetComponent<Rigidbody2D>();
+        _isGrounded = false;
     }
 
     private void Update() {
@@ -18,8 +25,32 @@ public class Player : MonoBehaviour
         vel.x = horizontal * moveSpeed;
         _rb.velocity = vel;
         
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space)) {        // if we pressed space
+            if (_isGrounded || _airJumpsLeft > 0) {    // if we are grounded or can air jump
+                _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);    // jump
+                if (!_isGrounded) {    // if we used an air jump
+                    _airJumpsLeft--;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        // 0001 0000 0010
+        // 0000 0000 1000
+        
+        // 0000 0000 0000
+        // other.layer = 8
+        // 1 left x
+        if (((1 << other.gameObject.layer) & groundLayers.value) != 0) {
+            _isGrounded = true;
+            _airJumpsLeft = maxAirJumps;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (((1 << other.gameObject.layer) & groundLayers.value) != 0) {
+            _isGrounded = false;
         }
     }
 }
